9596172b5297b11fdf3c24a0f85844f01004ba0e
JaspreetSingh <jaspreetsandhu1994@gmail.com>
2022-12-04 13:32:10 +1100

    Sorting Completed
    Sorting Completed

---
+++ lessons.js
@@ -0,0 +1,92 @@
+let dataJSON=[
+    {
+        "id": "7",
+        "subject": "Biology",
+        "location": "UK",
+        "price": "140",
+        "space": "5",
+        "icon": "fa-solid fa-book-open",
+        "image": "assets/images/book.png"
+    },
+    {
+        "id": "5",
+        "subject": "Chemistry",
+        "location": "India",
+        "price": "125",
+        "space": "5",
+        "icon": "fa-solid fa-book-open",
+        "image": "assets/images/book.png"
+    },
+    {
+        "id": "1",
+        "subject": "English",
+        "location": "London",
+        "price": "125",
+        "space": "5",
+        "icon": "fa-solid fa-book-open",
+        "image": "assets/images/book.png"
+    },
+    {
+        "id": "6",
+        "subject": "Japanese",
+        "location": "Japan",
+        "price": "130",
+        "space": "5",
+        "icon": "fa-solid fa-book-open",
+        "image": "assets/images/book.png"
+    },
+    {
+        "id": "3",
+        "subject": "Maths",
+        "location": "Canada",
+        "price": "110",
+        "space": "5",
+        "icon": "fa-solid fa-book-open",
+        "image": "assets/images/book.png"
+    },
+    {
+        "id": "10",
+        "subject": "Nursing",
+        "location": "Punjab",
+        "price": "145",
+        "space": "5",
+        "icon": "fa-solid fa-book-open",
+        "image": "assets/images/book.png"
+    },
+    {
+        "id": "4",
+        "subject": "Physics",
+        "location": "Australia",
+        "price": "115",
+        "space": "5",
+        "icon": "fa-solid fa-book-open",
+        "image": "assets/images/book.png"
+    },
+    {
+        "id": "8",
+        "subject": "Robotics",
+        "location": "Perth",
+        "price": "106",
+        "space": "5",
+        "icon": "fa-solid fa-book-open",
+        "image": "assets/images/book.png"
+    },
+    {
+        "id": "2",
+        "subject": "Spanish",
+        "location": "Derby",
+        "price": "120",
+        "space": "5",
+        "icon": "fa-solid fa-book-open",
+        "image": "assets/images/book.png"
+    },
+    {
+        "id": "9",
+        "subject": "Technology",
+        "location": "Ontario",
+        "price": "100",
+        "space": "5",
+        "icon": "fa-solid fa-book-open",
+        "image": "assets/images/book.png"
+    }
+];
